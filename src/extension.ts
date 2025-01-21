9a702eececb34f6b365afad0cc8502c384c91168
Anshul Aggarwal <anshulaggarw4l@gmail.com>
2025-01-20 19:49:24 -0500

    showing menu

--- src/extension.ts
+++ src/extension.ts
@@ -1,12 +1,56 @@
 import * as vscode from "vscode";
 
+interface EditorQuickPickItem extends vscode.QuickPickItem {
+  uri: vscode.Uri;
+}
+
 export function activate(context: vscode.ExtensionContext) {
-	console.log('ContextClip is now active!');
   let disposable = vscode.commands.registerCommand(
     "contextclip.copyOpenEditors",
-    () => {
-      // For now, just show a notification to confirm it works
-      vscode.window.showInformationMessage("ContextClip: Command executed!");
+    async () => {
+      // Get all tab groups and their tabs
+      const allTabs = vscode.window.tabGroups.all.flatMap(group => 
+        group.tabs.filter(tab => tab.input instanceof vscode.TabInputText)
+          .map(tab => (tab.input as vscode.TabInputText).uri)
+      );
+      
+      // If no editors are open, show a message and return
+      if (allTabs.length === 0) {
+        vscode.window.showInformationMessage("No editors are currently open.");
+        return;
+      }
+
+      // Create QuickPick items from tabs
+      const items: EditorQuickPickItem[] = allTabs.map(uri => {
+        return {
+          label: vscode.workspace.asRelativePath(uri),
+          description: uri.fsPath,
+          uri: uri
+        };
+      });
+
+      // Create and show QuickPick
+      const quickPick = vscode.window.createQuickPick<EditorQuickPickItem>();
+      quickPick.items = items;
+      quickPick.placeholder = "Select editors to copy (use space to select multiple)";
+      quickPick.canSelectMany = true;
+
+      quickPick.onDidAccept(() => {
+        const selectedItems = quickPick.selectedItems;
+        if (selectedItems.length > 0) {
+          // For now, just show selected files (we'll implement copying in next phase)
+          const fileList = selectedItems
+            .map(item => item.label)
+            .join("\n");
+          vscode.window.showInformationMessage(
+            `Selected files:\n${fileList}`
+          );
+        }
+        quickPick.hide();
+      });
+
+      quickPick.onDidHide(() => quickPick.dispose());
+      quickPick.show();
     }
   );
 
